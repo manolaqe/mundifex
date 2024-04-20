de7fc22ea510111b76f8350e1cbf85a7042e33bb
manolaqe <manolachemihaiandrei@protonmail.ch>
2024-04-20 16:08:33 +0300

    added github workflow

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Mundifex',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrangeAccent),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Mundifex'),
